4992e43754b22f97001df1de393fcc118c3ef919
nadafeniche00-hub <nfe15rajab@gmail.com>
2026-02-24 18:04:25 -0500

    task 2

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -8,14 +8,14 @@ from .models import Course, Lesson, Instructor, Learner, Question, Choice
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
-
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 5
 class QuestionInline(admin.StackedInline):
     model = Question
     extra = 5
 
-class ChoiceInline(admin.StackedInline):
-    model = Choice
-    extra = 5
+
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -39,3 +39,4 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Submission)
